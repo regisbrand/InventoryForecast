2c1fa6d15f5734a48b1ef7269d72ae1759c66672
Regis Brand <regis.brand@poatek.com>
2016-06-03 13:38:32 -0400

    first version

---
+++ InventoryForecast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InventoryForecast.Startup))]
+namespace InventoryForecast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
